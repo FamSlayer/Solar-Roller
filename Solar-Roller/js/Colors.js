133f376efab0d02c7dc9de0b19c7bdafcec849ca
swood456 <crazedsportsfan@gmail.com>
2016-12-08 15:45:04 -0500

    forgot to add new folder

---
+++ Solar-Roller/js/Colors.js
@@ -0,0 +1,56 @@
+var Colors = function(numVerts){
+	
+	//make red
+	var colArr = [];
+	for(var i = 0; i <numVerts; i++){
+		colArr.push(255);
+		colArr.push(0);
+		colArr.push(0);
+	}
+	this.RED = new Uint8Array(colArr);
+
+	colArr = [];
+	for(var i = 0; i <numVerts; i++){
+		colArr.push(0);
+		colArr.push(0);
+		colArr.push(255);
+	}
+
+	this.BLUE = new Uint8Array(colArr);
+
+	colArr = [];
+	for(var i = 0; i <numVerts; i++){
+		colArr.push(0);
+		colArr.push(255);
+		colArr.push(0);
+	}
+
+	this.GREEN = new Uint8Array(colArr);
+
+	colArr = [];
+	for(var i = 0; i <numVerts; i++){
+		colArr.push(0);
+		colArr.push(255);
+		colArr.push(229);
+	}
+
+	this.CYAN = new Uint8Array(colArr);
+
+	colArr = [];
+	for(var i = 0; i <numVerts; i++){
+		colArr.push(255);
+		colArr.push(0);
+		colArr.push(229);
+	}
+
+	this.MAGENTA = new Uint8Array(colArr);
+
+	colArr = [];
+	for(var i = 0; i <numVerts; i++){
+		colArr.push(255);
+		colArr.push(255);
+		colArr.push(0);
+	}
+
+	this.YELLOW = new Uint8Array(colArr);
+};
